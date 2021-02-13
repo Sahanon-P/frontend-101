814c00c315c6c34f49e0e6ed94dcc8f72284dda2
numkhang <numkhang44@gmail.com>
2021-02-13 16:28:11 +0900

    edit slot

--- slot.js
+++ slot.js
@@ -10,122 +10,50 @@ const boxtext3=document.getElementById("boxtext3");
 const box4=document.getElementById("box4");
 const boxtext4=document.getElementById("boxtext4");
 
+const box=[box1,box2,box3,box4];
+const boxtxt=[boxtext1,boxtext2,boxtext3,boxtext4];
+
 let c=0;
 let min=0;
 let sec=0;
 
-function getslot1(){
-    fetch("",{
+
+function slot(){
+    for(i=0;i<4;i++){
+        getslot(i);
+    }
+}
+
+function getslot(i){
+    c=i+1;
+    fetch(`/parking?slot=${c}`,{
         method: "GET",
         headers: {"Content-Type": "application/json"},
     }).then((x) => x.json())
     .then((x) => {
-        if(x.parking_status==0){
-            box1.className="green_box";
-            boxtext1.className="green_txt";
-            boxtext1.innerHTML="Available"
+        if(x.parking_is_available==true){
+            box[i].className="green_box";
+            boxtext[i].className="green_txt";
+            boxtext[i].innerHTML="Available"
         }else{
-            box1.className="red_box";
-            boxtext1.className="red_txt";
-            int(min) = x.parking_time / 60 ;
-            int(sec) = x.parking_time % 60;
+            box[i].className="red_box";
+            boxtext[i].className="red_txt";
+            min = int(x.parking_time);
+            sec = int((x.parking_time - min)* 60);
             if(min < 10){
                 min=char("0"+Char(min));
             }
             if(sec < 10){
                 sec=char("0"+Char(sec));
             }
-            boxtext1.innerHTML=min+":"+sec+" MIN";
+            boxtext[i].innerHTML="TIME\n"+min+":"+sec+" MIN";
         }
     });
 }
-
-function getslot2(){
-    fetch("",{
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-    }).then((x) => x.json())
-    .then((x) => {
-        if(x.parking_status==0){
-            box2.className="green_box";
-            boxtext2.className="green_txt";
-            boxtext2.innerHTML="Available"
-        }else{
-            box2.className="red_box";
-            boxtext2.className="red_txt";
-            int(min) = x.parking_time / 60 ;
-            int(sec) = x.parking_time % 60;
-            if(min < 10){
-                min=char("0"+Char(min));
-            }
-            if(sec < 10){
-                sec=char("0"+Char(sec));
-            }
-            boxtext2.innerHTML="TIME\n"+min+":"+sec+" MIN";
-        }
-    });
-}
-
-function getslot3(){
-    fetch("",{
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-    }).then((x) => x.json())
-    .then((x) => {
-        if(x.parking_status==0){
-            box3.className="green_box";
-            boxtext3.className="green_txt";
-            boxtext3.innerHTML="Available"
-        }else{
-            box3.className="red_box";
-            boxtext3.className="red_txt";
-            int(min) = x.parking_time / 60 ;
-            int(sec) = x.parking_time % 60;
-            if(min < 10){
-                min=char("0"+Char(min));
-            }
-            if(sec < 10){
-                sec=char("0"+Char(sec));
-            }
-            boxtext3.innerHTML=min+":"+sec+" MIN";
-        }
-    });
-}
-
-function getslot4(){
-    fetch("",{
-        method: "GET",
-        headers: {"Content-Type": "application/json"},
-    }).then((x) => x.json())
-    .then((x) => {
-        if(x.parking_status==0){
-            box4.className="green_box";
-            boxtext4.className="green_txt";
-            boxtext4.innerHTML="Available"
-        }else{
-            box4.className="red_box";
-            boxtext4.className="red_txt";
-            int(min) = x.parking_time / 60 ;
-            int(sec) = x.parking_time % 60;
-            if(min < 10){
-                min=char("0"+Char(min));
-            }
-            if(sec < 10){
-                sec=char("0"+Char(sec));
-            }
-            boxtext4.innerHTML=min+":"+sec+" MIN";
-        }
-    });
-}
-
-
 
 
 
 setInterval(() => {
-    getslot1();
-    getslot2();
-    getslot3();
-    getslot4();
+    slot();
 }, 6000);
 
