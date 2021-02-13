2b4ba4cafeccb6c5ea0506ebe7d383edb70fee98
numkhang <numkhang44@gmail.com>
2021-02-13 14:59:53 +0900

    edit slot

--- slot.js
+++ slot.js
@@ -10,26 +10,39 @@ const boxtext3=document.getElementById("boxtext3");
 const box4=document.getElementById("box4");
 const boxtext4=document.getElementById("boxtext4");
 
+let c=0;
+let min=0;
+let sec=0;
 
-
-function getslot1(){
+function getslot(){
     fetch("",{
         method: "GET",
         headers: {"Content-Type": "application/json"},
     }).then((x) => x.json())
-    if(x.parking_status==0){
-        box1.className="green_box";
-        boxtext1.className="green_txt";
-        boxtext1.innerHTML="Available"
-    }else{
-        box1.className="red_box";
-        boxtext1.className="red_txt";
-        boxtext1.innerHTML=;
-    }
+    .then((x) => {
+        if(x.parking_status==0){
+            box1.className="green_box";
+            boxtext1.className="green_txt";
+            boxtext1.innerHTML="Available"
+        }else{
+            box1.className="red_box";
+            boxtext1.className="red_txt";
+            int(min) = x.parking_time / 60 ;
+            int(sec) = x.parking_time % 60;
+            if(min < 10){
+                min=char("0"+Char(min));
+            }
+            if(sec < 10){
+                sec=char("0"+Char(sec));
+            }
+            boxtext1.innerHTML=min+":"+sec+" MIN";
+        }
+    });
 }
+
+
 
 
 setInterval(() => {
     getslot1();
-
 }, 6000);
